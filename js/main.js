f45714f73e15dd790c1c41f36a7c52f53d52996e
fangjie <857516512@qq.com>
2022-02-18 10:23:37 +0800

    MinMarkdown-PPT

--- js/main.js
+++ js/main.js
@@ -1,23 +1,14 @@
-/*
- * @Author: your name
- * @Date: 2020-03-04 20:50:20
- * @LastEditTime: 2020-03-09 22:01:00
- * @LastEditors: Please set LastEditors
- * @Description: In User Settings Edit
- * @FilePath: /oneslide/css/main.js
- */
 const $ = s => document.querySelector(s)
 const $$ = s => document.querySelectorAll(s)
 const isMain = str => (/^#{1,2}(?!#)/).test(str)
 const isSub = str => (/^#{3}(?!#)/).test(str)
+//格式化Markdown格式
 const convert = raw => {
-  let arr = raw.split(/\n(?=\s*#{1,3}[^#])/).filter(s => s!="").map(s => s.trim())
-
+  let arr = raw.split(/\n(?=\s*#{1,3}[^#])/).filter(s => s != "").map(s => s.trim())
   let html = ''
-  for(let i=0; i<arr.length; i++) {
-
-    if(arr[i+1] !== undefined) {
-      if(isMain(arr[i]) && isMain(arr[i+1])) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i + 1] !== undefined) {
+      if (isMain(arr[i]) && isMain(arr[i + 1])) {
         html += `
 <section data-markdown>
 <textarea data-template>
@@ -25,7 +16,7 @@ ${arr[i]}
 </textarea>
 </section>
 `
-      } else if(isMain(arr[i]) && isSub(arr[i+1])) {
+      } else if (isMain(arr[i]) && isSub(arr[i + 1])) {
         html += `
 <section>
 <section data-markdown>
@@ -34,7 +25,7 @@ ${arr[i]}
 </textarea>
 </section>
 `
-      } else if(isSub(arr[i]) && isSub(arr[i+1])) {
+      } else if (isSub(arr[i]) && isSub(arr[i + 1])) {
         html += `
 <section data-markdown>
 <textarea data-template>
@@ -42,7 +33,7 @@ ${arr[i]}
 </textarea>
 </section>
 `
-      } else if(isSub(arr[i]) && isMain(arr[i+1])) {
+      } else if (isSub(arr[i]) && isMain(arr[i + 1])) {
         html += `
 <section data-markdown>
 <textarea data-template>
@@ -51,18 +42,18 @@ ${arr[i]}
 </section>
 </section>
 `
-      }      
+      }
 
     } else {
-      if(isMain(arr[i])) {
+      if (isMain(arr[i])) {
         html += `
 <section data-markdown>
 <textarea data-template>
 ${arr[i]}
 </textarea>
 </section>
-`        
-      } else if(isSub(arr[i])) {
+`
+      } else if (isSub(arr[i])) {
         html += `
 <section data-markdown>
 <textarea data-template>
@@ -70,17 +61,16 @@ ${arr[i]}
 </textarea>
 </section>
 </section>
-`        
+`
       }
     }
-
   }
   return html
 }
 
-
+// 显示切换面板
 const Menu = {
-  init() {
+  init () {
     console.log('Menu init...')
     this.$settingIcon = $('.control .icon-setting')
     this.$menu = $('.menu')
@@ -91,7 +81,7 @@ const Menu = {
     this.bind()
   },
 
-  bind() {
+  bind () {
     this.$settingIcon.onclick = () => {
       this.$menu.classList.add('open')
     }
@@ -110,9 +100,9 @@ const Menu = {
   }
 }
 
-
+// 上传图片
 const ImgUploader = {
-  init() {
+  init () {
     this.$fileInput = $('#img-uploader')
     this.$textarea = $('.editor textarea')
 
@@ -125,21 +115,21 @@ const ImgUploader = {
     this.bind()
   },
 
-  bind() {
+  bind () {
     let self = this
-    this.$fileInput.onchange = function() {
+    this.$fileInput.onchange = function () {
       if (this.files.length > 0) {
         let localFile = this.files[0]
         console.log(localFile)
-        if(localFile.size/1048576 > 2) {
+        if (localFile.size / 1048576 > 2) {
           alert('文件不能超过2M')
           return
         }
         self.insertText(`![上传中，进度0%]()`)
-        let  avFile = new AV.File(encodeURI(localFile.name), localFile)
-        avFile.save({ 
-          keepFileName: true, 
-          onprogress(progress) {
+        let avFile = new AV.File(encodeURI(localFile.name), localFile)
+        avFile.save({
+          keepFileName: true,
+          onprogress (progress) {
             self.insertText(`![上传中，进度${progress.percent}%]()`)
           }
         }).then(file => {
@@ -148,11 +138,11 @@ const ImgUploader = {
           let text = `![${file.attributes.name}](${file.attributes.url}?imageView2/0/w/800/h/400)`
           self.insertText(text)
         }).catch(err => console.log(err))
-       }     
+      }
     }
   },
 
-  insertText(text = '') {
+  insertText (text = '') {
     let $textarea = this.$textarea
     let start = $textarea.selectionStart
     let end = $textarea.selectionEnd
@@ -160,13 +150,13 @@ const ImgUploader = {
 
     $textarea.value = `${oldText.substring(0, start)}${text} ${oldText.substring(end)}`
     $textarea.focus()
-    $textarea.setSelectionRange(start, start + text.length) 
+    $textarea.setSelectionRange(start, start + text.length)
   }
 }
 
-
+// 设置转场
 const Editor = {
-  init() {
+  init () {
     console.log('Editor init...')
     this.$editInput = $('.editor textarea')
     this.$saveBtn = $('.editor .button-save')
@@ -177,38 +167,38 @@ const Editor = {
     this.start()
   },
 
-  bind() {
+  bind () {
     this.$saveBtn.onclick = () => {
       localStorage.markdown = this.$editInput.value
       location.reload()
     }
   },
 
-  start() {
+  start () {
     this.$editInput.value = this.markdown
     this.$slideContainer.innerHTML = convert(this.markdown)
     Reveal.initialize({
-          controls: true,
-          progress: true,
-          center: localStorage.align === 'left-top' ? false : true,
-          hash: true,
-          transition: localStorage.transition || 'slide', // none/fade/slide/convex/concave/zoom
-          // More info https://github.com/hakimel/reveal.js#dependencies
-          dependencies: [
-            { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-            { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
-            { src: 'plugin/highlight/highlight.js' },
-            { src: 'plugin/search/search.js', async: true },
-            { src: 'plugin/zoom-js/zoom.js', async: true },
-            { src: 'plugin/notes/notes.js', async: true }
-          ]
-        })
+      controls: true,
+      progress: true,
+      center: localStorage.align === 'left-top' ? false : true,
+      hash: true,
+      transition: localStorage.transition || 'slide', // none/fade/slide/convex/concave/zoom
+      // More info https://github.com/hakimel/reveal.js#dependencies
+      dependencies: [
+        { src: 'plugin/markdown/marked.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+        { src: 'plugin/markdown/markdown.js', condition: function () { return !!document.querySelector('[data-markdown]'); } },
+        { src: 'plugin/highlight/highlight.js' },
+        { src: 'plugin/search/search.js', async: true },
+        { src: 'plugin/zoom-js/zoom.js', async: true },
+        { src: 'plugin/notes/notes.js', async: true }
+      ]
+    })
   }
 }
 
-
+// 设置主题CSS
 const Theme = {
-  init() {
+  init () {
     this.$$figures = $$('.theme figure')
     this.$transition = $('.theme .transition')
     this.$align = $('.theme .align')
@@ -218,30 +208,30 @@ const Theme = {
     this.loadTheme()
   },
 
-  bind() {
+  bind () {
     this.$$figures.forEach($figure => $figure.onclick = () => {
       this.$$figures.forEach($item => $item.classList.remove('select'))
       $figure.classList.add('select')
       this.setTheme($figure.dataset.theme)
     })
 
-    this.$transition.onchange = function() {
+    this.$transition.onchange = function () {
       localStorage.transition = this.value
       location.reload()
     }
 
-    this.$align.onchange = function() {
+    this.$align.onchange = function () {
       localStorage.align = this.value
       location.reload()
     }
   },
 
-  setTheme(theme) {
+  setTheme (theme) {
     localStorage.theme = theme
     location.reload()
   },
 
-  loadTheme() {
+  loadTheme () {
     let theme = localStorage.theme || 'beige'
     let $link = document.createElement('link')
     $link.rel = 'stylesheet'
@@ -255,16 +245,16 @@ const Theme = {
     this.$reveal.classList.add(this.$align.value)
   }
 }
-
-const Print =  {
-  init() {
+// 打印模块
+const Print = {
+  init () {
     this.$download = $('.download')
 
     this.bind()
     this.start()
   },
 
-  bind() {
+  bind () {
     this.$download.addEventListener('click', () => {
       let $link = document.createElement('a')
       $link.setAttribute('target', '_blank')
@@ -278,11 +268,11 @@ const Print =  {
     }
   },
 
-  start() {
+  start () {
     let link = document.createElement('link')
     link.rel = 'stylesheet'
     link.type = 'text/css'
-    if(window.location.search.match(/print-pdf/gi)) {
+    if (window.location.search.match(/print-pdf/gi)) {
       link.href = 'css/print/pdf.css'
       window.print()
     } else {
@@ -293,7 +283,7 @@ const Print =  {
 }
 
 const App = {
-  init() {
+  init () {
     [...arguments].forEach(Module => Module.init())
   }
 }
